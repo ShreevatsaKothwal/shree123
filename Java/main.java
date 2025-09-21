cd2a67e73dbd1c181e9f073c57a18284129ab84e
ShreevatsaKothwal <145187011+ShreevatsaKothwal@users.noreply.github.com>
2025-09-22 00:06:41 +0530

    Add files via upload

---
+++ Java/main.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public  class main{
+
+    public static void printMyname(string name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args){ 
+
+       Scanner sc = new Scanner(System.in);
+        
+       string name=sc.next()
+
+            
+     }
+
+}
+
+
+
